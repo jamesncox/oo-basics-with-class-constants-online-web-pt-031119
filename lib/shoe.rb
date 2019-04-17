5be8f954c85fb4ba74647f44aa05025e13f69d97
James Cox <jamesncox@yahoo.com>
2019-04-17 18:21:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,14 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+<<<<<<< HEAD
     BRANDS << brand unless BRANDS.include?(brand)
+=======
+    if @brand == brand.uniq
+      BRANDS << brand
+    else 
+      BRANDS
+>>>>>>> fce85dc12b1e8f3013ae37c3ae5f39839aa7c5e2
   end
 
   def cobble
